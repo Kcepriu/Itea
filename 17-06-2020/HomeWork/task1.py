9fb5d3421b95d4011a2cf0f71c01e171d809b545
Kcepriu <kcepriu@gmail.com>
2020-06-18 12:15:18 +0300

    Add task

--- 17-06-2020/HomeWork/task1.py
+++ 17-06-2020/HomeWork/task1.py
@@ -3,7 +3,7 @@
 классе базовые аттрибуты для автомобилей. Будет плюсом если в
 классах наследниках переопределите методы базового класса."""
 
-class Car:
+class Car_common:
     def __init__(self, vin, make, year, owner = '', price=0):
         #Антон спитаю тут. А нема якихось рекомендацій, чи загальноприйнятих правил по передачі параметрів в клас?
 
@@ -30,32 +30,43 @@ class Car:
         print('Owner -', self.owner)
         print('Price -', self.price)
 
-class Truck(Car):
+class Car(Car_common):
+    def __init__(self, vin, make, year, owner = '', price=0, body_style='sedan'):
+        Car_common.__init__(self, vin, make, year, owner, price)
+        self.body_style = body_style
+
+    def print_info(self):
+        Car_common.print_info(self)
+        print('Body style -', self.body_style)
+
+
+
+class Truck(Car_common):
     def __init__(self, vin, make, year, owner = '', price=0, max_carrying=0):
-        Car.__init__(self, vin, make, year, owner, price)
+        Car_common.__init__(self, vin, make, year, owner, price)
         self.max_carrying = max_carrying
 
     def change_the_price(self, percent=5):
         '''По замовчанню на інший відсоток зміними ціну'''
-        Car.change_the_price(self, percent)
+        Car_common.change_the_price(self, percent)
 
     def print_info(self):
-        Car.print_info(self)
+        Car_common.print_info(self)
         print('Max carrying -', self.max_carrying)
 
 
+if __name__ == '__main__':
+    car1 = Car('123456', 'Ford', '2012', '', 5000)
 
-car1 = Car('123456', 'Ford', '2012', '', 5000)
 
+    car1.print_info()
+    car1.new_owner('Julli')
+    car1.print_info()
+    car1.change_the_price(10)
+    car1.print_info()
 
-car1.print_info()
-car1.new_owner('Julli')
-car1.print_info()
-car1.change_the_price(10)
-car1.print_info()
+    car2 = Truck('98765', 'Volvo', '2019', '', 80000, 20000)
 
-car2 = Truck('98765', 'Volvo', '2019', '', 80000, 20000)
-
-car2.print_info()
-car2.change_the_price()
-car2.print_info()
+    car2.print_info()
+    car2.change_the_price()
+    car2.print_info()
