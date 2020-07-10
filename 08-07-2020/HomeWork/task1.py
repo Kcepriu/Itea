6571572cfc6e0a02e84dc086a8bf4ad817ddff79
sergiy <kcepriu@gmail.com>
2020-07-10 22:33:37 +0300

    08-07-2020

--- 08-07-2020/HomeWork/task1.py
+++ 08-07-2020/HomeWork/task1.py
@@ -5,10 +5,13 @@ class MyError(Exception):
 
 class ErrorAutentification(MyError):
     pass
+
 class ErrorNoFindUser(ErrorAutentification):
     pass
+
 class ErrorWrongPassword(ErrorAutentification):
     pass
+
 class ErrorNotLogining(ErrorAutentification):
     pass
 
@@ -16,6 +19,9 @@ class ErrorNotAccess(ErrorAutentification):
     pass
 
 class NoFindId(MyError):
+    pass
+
+class UserWithLoginIsRegistered(ErrorAutentification):
     pass
 
 
@@ -26,14 +32,17 @@ class Autentification:
     def read_data_from_user(self, func, *args):
         return func(*args)
 
+    def update_user(self, func, *args):
+        func(*args)
 
 class Logining(Autentification):
-    def __init__(self, func_read, func_write):
+    def __init__(self, func_read, func_write, func_update):
         self._login = None
         self._passwords = None
         self._admin = None
         self._func_read = func_read
         self._func_write = func_write
+        self._func_update = func_update
 
     @property
     def is_admin(self):
@@ -48,7 +57,6 @@ class Logining(Autentification):
         self._passwords = None
         self._admin = None
 
-
     def loggin(self, login, passwords):
         data = self.read_data_from_user(self._func_read, login)
 
@@ -62,7 +70,6 @@ class Logining(Autentification):
         else:
             raise ErrorWrongPassword
 
-
     def add_user(self, login, passwords, admin):
         if not self.logged:
             raise ErrorNotLogining
@@ -70,8 +77,30 @@ class Logining(Autentification):
         if not self.is_admin:
             raise ErrorNotAccess
 
+        if  self.read_data_from_user(self._func_read, login):
+            raise UserWithLoginIsRegistered
 
         super().add_user(self._func_write, login, passwords, admin)
+
+    def update_user(self, login, new_passorsd, new_admin):
+        if not self.logged:
+            raise ErrorNotLogining
+
+        user_info = self.read_data_from_user(self._func_read, login)
+
+        if not user_info:
+            raise ErrorNoFindUser
+
+        #якщо не адмін то може змінювати тільки свій пароль
+        #Адмін може змінити як пороль так і ролдь будь кого
+        if not self.is_admin:
+            if self._login != login:
+                raise ErrorNotAccess
+
+            if new_admin:
+                raise ErrorNotAccess
+
+        super().update_user(self._func_update, login, new_passorsd, new_admin)
 
 
 class ConnectDatabase:
@@ -114,17 +143,20 @@ class InitialData:
                             login	TEXT UNIQUE,
                             passwords	TEXT,
                             admin	NUMERIC DEFAULT 0)''',
-                            'INSERT  INTO users (login, passwords, admin)  VALUES (?, ?, ?)'),
+                            'INSERT  INTO users (login, passwords, admin)  VALUES (?, ?, ?)',
+                             ''),
 
                     'faculties': ('''CREATE TABLE faculties (
                                         id	INTEGER PRIMARY KEY AUTOINCREMENT,
                                         name_faculty	TEXT UNIQUE)''',
-                                 'INSERT INTO faculties (name_faculty)  VALUES (?)'),
+                                 'INSERT INTO faculties (name_faculty)  VALUES (?)',
+                             ''),
 
                     'gpoups': ('''CREATE TABLE gpoups (
                                     id	INTEGER PRIMARY KEY AUTOINCREMENT,
                                     name_group	INTEGER UNIQUE)''',
-                               'INSERT INTO gpoups (name_group)  VALUES (?)'),
+                               'INSERT INTO gpoups (name_group)  VALUES (?)',
+                             ''),
 
                     'students': ('''CREATE TABLE students (
                                         id	INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -133,7 +165,10 @@ class InitialData:
                                         id_group	INTEGER NOT NULL,
                                         student_number	INTEGER NOT NULL UNIQUE,
                                         FOREIGN KEY(id_faculty) REFERENCES facultis(id))''',
-                            'INSERT INTO students (name, id_faculty, id_group, student_number)  VALUES (?, ?, ?, ?)'),
+                            'INSERT INTO students (name, id_faculty, id_group, student_number)  VALUES (?, ?, ?, ?)',
+                             '''UPDATE students
+                                    SET name = ?, id_faculty = ?, id_group = ?, student_number = ? 
+                                WHERE id = ?'''),
 
                     'mark_student': ('''CREATE TABLE mark_student (
                                             id	INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -141,11 +176,13 @@ class InitialData:
                                             mark	INTEGER NOT NULL,
                                             id_item	INTEGER NOT NULL,
                                             FOREIGN KEY(id_student) REFERENCES students(id))''',
-                                     'INSERT INTO mark_student (id_student, id_item, mark)  VALUES (?, ?, ?)'),
+                                     'INSERT INTO mark_student (id_student, id_item, mark)  VALUES (?, ?, ?)',
+                             ''),
                       'items':('''CREATE TABLE items (
                                 id	INTEGER PRIMARY KEY AUTOINCREMENT,
                                 item_name	TEXT NOT NULL UNIQUE) ''',
-                               'INSERT INTO items (item_name)  VALUES (?)')   }
+                               'INSERT INTO items (item_name)  VALUES (?)',
+                             '')   }
 
     _test_data ={'faculties':['Філологічний', 'Психологічний', 'Педагогічний'],
 
@@ -191,14 +228,30 @@ class InitialData:
                                  ]
                  }
 
+class Students:
+    def __init__(self, id, name, id_faculty, id_group, student_number):
+        self._id = 0
+        self._name = name
+        self._id_faculty = id_faculty
+        self._id_group = id_group
+        self._student_number = student_number
+
+    def write_data(self, func_write):
+       func_write(id=self._id, name=self._name, id_faculty=self._id_faculty,
+                   id_group=self._id_group, student_number=self._student_number)
+
+    def __str__(self):
+        return f'Student name: {self._name}\n\tID faculty: {self._id_faculty}\n\tId group: {self._id_group}\n\tStudent numbers: {self._student_number}'
 
 class DatabaseStudents(Logining, InitialData):
     def __init__(self, database_name):
         self._database_name = database_name
-        super().__init__(self.read_user, self.write_user)
+        super().__init__(self.read_user, self.write_user, self.update_user)
+        self._curent_student = None
+
 
     def read_user(self, login):
-        sql_text = 'select  login, passwords, admin from users where login = ?'
+        sql_text = 'select  id, login, passwords, admin from users where login = ?'
         result_sql = self._execute_sql(sql_text, [login])
 
         result = None
@@ -208,8 +261,12 @@ class DatabaseStudents(Logining, InitialData):
 
         return result
 
-    def write_user(self):
-        pass
+    def write_user(self, login, passwords, admin):
+        self._execute_sql(self._structure_tables['users'][1], [login, passwords, 1 if admin else 0])
+
+    def update_user(self, login, new_passorsd, new_admin):
+        #треба дописати
+        print('Не реалізував')
 
     def _table_exists(self, table_name):
         #Перевіряю чи існує таблиця в  базі
@@ -271,6 +328,19 @@ class DatabaseStudents(Logining, InitialData):
                     #Помилка випадає через унікальність запису. Значить такі дані вже внесені. Ігноруємо
                     pass
 
+    def write_student(self, **kwargs):
+        print(kwargs['id'])
+        if id:
+            self._execute_sql(self._structure_tables['students'][1],
+                              [kwargs['name'], kwargs['id_faculty'], kwargs['id_group'], kwargs['student_number']])
+        else:
+            self._execute_sql(self._structure_tables['students'][2],
+                    [kwargs['name'], kwargs['id_faculty'], kwargs['id_group'], kwargs['student_number'], kwargs['id']])
+
+    def add_studetn(self, name, id_faculty, id_group, student_number):
+        student = Students(0, name, id_faculty, id_group, student_number)
+        student.write_data(self.write_student)
+
 if __name__ == '__main__':
     students_databases = DatabaseStudents("students_db_new.db")
     students_databases._initial_tables()
@@ -278,13 +348,16 @@ if __name__ == '__main__':
 
     # print(students_databases._login)
 
-    # students_databases.loggin('admin', 'admin')
-    students_databases.loggin('user', 'user')
-    print(students_databases.is_admin)
-    print(students_databases.logged)
-    print(students_databases._login)
-    students_databases.logout()
-    print(students_databases.logged)
+    students_databases.loggin('admin', 'admin')
+    # students_databases.loggin('user', 'user')
+    # print(students_databases.is_admin)
+    # print(students_databases.logged)
+    # print(students_databases._login)
+    # students_databases.logout()
+    # print(students_databases.logged)
+    # students_databases.add_user('ks_admin', '123456', True)
+
+    students_databases.add_studetn('Student 1', 1, 2, 125464)
 
 
 
